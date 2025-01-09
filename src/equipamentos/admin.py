86bd55282b93733b6edb9ebf57bf9986955401ad
devtvc <raulsilva@tvcultura.com.br>
2025-01-09 12:19:14 -0300

    This is a new commit for what I originally planned to be amended

--- src/equipamentos/admin.py
+++ src/equipamentos/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import Dados_Equipamentos
 
 # Register your models here.
+# TESTEEEE
 admin.site.register(Dados_Equipamentos)
--- src/estacao/views.py
+++ src/estacao/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Estacao
 import json
 
 # Create your views here.
 def estacao_page(request):
-    # station_count = Estacao.objects.count()
+    station_count = Estacao.objects.count()
+    context = {
+        "title": "Sistema de gerência da rede do interior",
+        "content": f"Número de Estações TV Cultura: { station_count }",
+    }
     estacoes = ['TV Cultura', 'TV Cultura Paulista', 'TV Cultura Brasília']
     valores = [10, 20, 30]
-    return render(request, 'estacoes.html', {
-        'station_count': len(estacoes),
+    context.update({        
         'estacoes': json.dumps(estacoes),
-        'valores': json.dumps(valores),
-        })
+        'valores': json.dumps(valores),})
+    
+    return render(request, 'estacoes.html', context)
 
 def image_view(request):
     # Your logic to serve an image or list images here
