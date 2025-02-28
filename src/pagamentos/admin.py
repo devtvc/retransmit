f703097a8b6c66fecbaefb2084191fc2c1c04fd0
devtvc <raulsilva@tvcultura.com.br>
2025-02-28 17:09:11 -0300

    change pagamentos

--- src/pagamentos/admin.py
+++ src/pagamentos/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Dados_Pagamento
 
+
 class Dados_PagamentoAdmin(admin.ModelAdmin):
-    list_display = ('cidade', 'tipo_conta', 'mes_referencia')  # Columns to display in the admin list
+    list_display = ('cidade', 'tipo_conta', 'mes_referencia', 'valor_liquido_conta')  # Columns to display in the admin list
     list_filter = ('tipo_conta', 'mes_referencia')  # Fields for filtering
     search_fields = ('cidade', 'tipo_conta')  # Fields to include in the search
-
+    
 # Register your models here.
 admin.site.register(Dados_Pagamento, Dados_PagamentoAdmin)
--- src/pagamentos/models.py
+++ src/pagamentos/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from IBGE.models import Dados_IBGE
+from django.db.models import Sum
 
 # Create your models here.
 class Dados_Pagamento(models.Model):
-    #cidade = models.CharField(max_length=100)
-    #uf = models.CharField(max_length=10)
+    
     def get_cidade_choices():
-         # Fetch choices dynamically from the Cidade model
+        
         unique_cidades = Dados_IBGE.objects.values_list('cidade', flat=True).distinct()
         return [(cidade, cidade) for cidade in unique_cidades]
     
@@ -17,22 +17,11 @@ class Dados_Pagamento(models.Model):
         null=True,
     )
     
-    # def get_uf_choices():
-    #      # Fetch choices dynamically from the Cidade model
-    #     unique_ufs = Dados_IBGE.objects.values_list('uf', flat=True).distinct()
-    #     return [(uf, uf) for uf in unique_ufs]
-    
-    # uf = models.CharField(
-    #     max_length=100,
-    #     choices=get_uf_choices,
-    #     blank=True,
-    #     null=True,
-    # )
     UFs = [
         ('item1', 'SP'),
         ('item2', 'DF'),
     ]
-    uf = models.CharField(max_length=10, choices=UFs, default='item1')
+    uf = models.CharField(max_length=10, choices=UFs, default='item1', verbose_name="UF")
 
 
     PAGO_POR = [
@@ -46,9 +35,14 @@ class Dados_Pagamento(models.Model):
         ('item3', 'ALUGUEL'),
         ('item4', 'IPTU'),
         ('item5', 'TELEFONE'),
+        ('item6', 'MÃO DE OBRA'),
+        ('item7', 'SERVIÇOS EVENTUAIS'),
+        ('item8', 'CONTRATOS'),
+        ('item9', 'CRÉDITO ROTATIVO (CAIXINHA)'),
+        ('item10', 'OUTROS - DETALHAR EM COMENTÁRIOS'),
     ]
 
-    tipo_conta = models.CharField(max_length=100, choices=TIPO_CONTA, default='item1')
+    tipo_conta = models.CharField(max_length=100, choices=TIPO_CONTA, default='item1', verbose_name="Tipo da conta")
     
 
     MESES = [
@@ -69,14 +63,15 @@ class Dados_Pagamento(models.Model):
         max_length=2,
         choices=MESES,
         blank=True,  
-        null=True, 
+        null=True,
+        verbose_name="Mês de referência" 
     )
-    conta_paga_a = models.CharField(max_length=100, help_text="Digite o CNPJ ou CPF")
-    razao_social_receptor = models.CharField(max_length=100, default="", help_text="Digite a razão social do CPF/CNPJ informado acima")
-    valor_bruto_conta = models.FloatField(null=True, blank=True, help_text="Digite o valor bruto da conta em R$")
-    valor_liquido_conta = models.FloatField(null=True, blank=True, help_text="Digite o valor líquido da conta em R$")
+    conta_paga_a = models.CharField(max_length=100, help_text="Digite o CNPJ ou CPF", verbose_name="Conta paga à (CNPJ):")
+    razao_social_receptor = models.CharField(max_length=100, default="", help_text="Digite a razão social do CPF/CNPJ informado acima", verbose_name="Razão social do receptor")
+    valor_bruto_conta = models.FloatField(null=True, blank=True, help_text="Digite o valor bruto da conta em R$", verbose_name="Valor bruto da conta")
+    valor_liquido_conta = models.FloatField(null=True, blank=True, help_text="Digite o valor líquido da conta em R$", verbose_name="Valor líquido da conta")
     conta_digitalizada = models.FileField(upload_to='pdfs/', blank=True, null=True, verbose_name="Conta digitalizada (PDF)")
-    comentarios = models.TextField(null=True, blank=True, help_text="Escreva outras observações da conta. Ex.: Número de contrato, consumo no mês etc")
+    comentarios = models.TextField(null=True, blank=True, help_text="Escreva outras observações da conta. Ex.: Número de contrato, consumo no mês etc", verbose_name="Comentários")
    
     class Meta:
         ordering = ['-mes_referencia']
@@ -85,5 +80,4 @@ class Dados_Pagamento(models.Model):
         verbose_name_plural = "Dados Pagamentos"  # Plural name
 
     def __str__(self):
-        
         return f"{self.cidade}"
